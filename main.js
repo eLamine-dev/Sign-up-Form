57c2c743b41313200ac6bd0ee063860752cc8d41
e-lamine <lamine@e-lamine.dev>
2022-12-27 18:33:19 +0100

    added regex validations

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const form = document.querySelector(".form");
-const pass_reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$/;
-const names_reg = /^[-'a-zA-Z]{2,12}$/;
-const email_reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
+let form = document.querySelector(".form");
 let firstName = form.elements.namedItem("first-name");
 let lastName = form.elements.namedItem("last-name");
 let email = form.elements.namedItem("email");
 let phoneNum = form.elements.namedItem("phone-num");
 let password = form.elements.namedItem("password");
-let passConfirm = form.elements.namedItem("confirm-pas");
+let passConfirm = form.elements.namedItem("confirm-pass");
+let reg = {
+   "first-name": /^[-'a-zA-Z]{2,15}$/,
+   "last-name": /^[-'a-zA-Z]{2,15}$/,
+   email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+   "phone-num":
+      /((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))/,
+   password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$/,
+   // "confirm-pass": new RegExp('\W*('  ")\W", "g"),
+};
 
 firstName.addEventListener("input", validate);
 lastName.addEventListener("input", validate);
@@ -16,35 +21,26 @@ email.addEventListener("input", validate);
 phoneNum.addEventListener("input", validate);
 password.addEventListener("input", validate);
 passConfirm.addEventListener("input", validate);
-
-function validate(e, reg) {
-   if (e.target.name == "password") {
-      if (pass_reg.test(e.target.value)) {
-         e.target.classList.add("valid");
-         e.target.classList.remove("invalid");
-      } else {
-         e.target.classList.add("invalid");
-         e.target.classList.remove("valid");
-      }
+passConfirm.addEventListener("input", (e) => {
+   if (passConfirm.value === password.value) {
+      e.target.classList.add("valid");
+      e.target.classList.remove("invalid");
+      e.target.nextElementSibling.style.visibility = "hidden";
+   } else {
+      e.target.classList.add("invalid");
+      e.target.classList.remove("valid");
+      e.target.nextElementSibling.style.visibility = "visible";
    }
+});
 
-   if (e.target.name == "first-name" || e.target.name == "last-name") {
-      if (names_reg.test(e.target.value)) {
-         e.target.classList.add("valid");
-         e.target.classList.remove("invalid");
-      } else {
-         e.target.classList.add("invalid");
-         e.target.classList.remove("valid");
-      }
-   }
-
-   if (e.target.name == "email") {
-      if (email_reg.test(e.target.value)) {
-         e.target.classList.add("valid");
-         e.target.classList.remove("invalid");
-      } else {
-         e.target.classList.add("invalid");
-         e.target.classList.remove("valid");
-      }
+function validate(e) {
+   if (reg[e.target.name].test(e.target.value)) {
+      e.target.classList.add("valid");
+      e.target.classList.remove("invalid");
+      e.target.nextElementSibling.style.visibility = "hidden";
+   } else {
+      e.target.classList.add("invalid");
+      e.target.classList.remove("valid");
+      e.target.nextElementSibling.style.visibility = "visible";
    }
 }
